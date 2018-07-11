b269a2246ff1b8885db2ac34484db2c14a63c1b7
netzebandito <blake.netzeband@gmail.com>
2018-07-11 17:31:41 -0600

    added babel. added dir for images and css assets

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
-#PORTFOLIO
+# PORTFOLIO
 
-Look at all the cool stuff I've done. Built with Webpack 4 and Vue.js
+Look at all the cool stuff I've done.
+
+Built with Webpack 4 and Vue.js
 
 See it live in action on the internet at [Blake Netzeband . com](https://www.blakenetzeband.com)
--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,47 @@
+const webpack = require('webpack')
 const { VueLoaderPlugin } = require('vue-loader')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
 
 module.exports = {
   mode: 'development',
   entry: [
     './src/main.js'
   ],
+  devServer: {
+    hot: true,
+    watchOptions: {
+      poll: true
+    }
+  },
   module: {
     rules: [
       {
         test: /\.vue$/,
         use: 'vue-loader'
-      }
+      },
+      {
+        test: /\.js$/,
+        use: 'babel-loader'
+      },
+      {
+        test: /\.css$/,
+        use: ['vue-style-loader', 'css-loader']
+      },
     ]
   },
   plugins: [
-    new VueLoaderPlugin()
+    new webpack.HotModuleReplacementPlugin(),
+    new VueLoaderPlugin(),
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: 'index.html',
+      inject: true
+    }),
+    new CopyWebpackPlugin([{
+      from: 'assets/images',
+      to: 'dist/assets/images',
+      toType: 'dir'
+    }])
   ]
 }

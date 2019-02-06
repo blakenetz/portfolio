1427a0580f11dd5909ab4b284a1742b344162193
Blake Netzeband <blake.netzeband@betsol.com>
2019-02-06 10:06:42 -0500

    added bg color to project text. fixed canvas width issue.

--- webpack.config.js
+++ webpack.config.js
@@ -1,56 +1,51 @@
-const webpack = require('webpack')
-const { VueLoaderPlugin } = require('vue-loader')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
-const CopyWebpackPlugin = require('copy-webpack-plugin')
+const webpack = require("webpack");
+const { VueLoaderPlugin } = require("vue-loader");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 module.exports = {
-  entry: [
-    './src/main.js'
-  ],
-  devServer: {
-    hot: true,
-    watchOptions: {
-      poll: true
-    }
-  },
-  module: {
-    rules: [
-      {
-        test: /\.vue$/,
-        use: 'vue-loader'
-      },
-      {
-        test: /\.js$/,
-        use: 'babel-loader'
-      },
-      {
-        test: /\.css$/,
-        use: ['vue-style-loader', 'css-loader']
-      },
-    ]
-  },
-  plugins: [
-    new webpack.HotModuleReplacementPlugin(),
-    new VueLoaderPlugin(),
-    new HtmlWebpackPlugin({
-      filename: 'index.html',
-      template: 'index.html',
-      inject: true
-    }),
-    new HtmlWebpackPlugin({
-      filename: 'error.html',
-      template: 'error.html',
-      inject: true
-    }),
-    new CopyWebpackPlugin([{
-      from: 'assets/images',
-      to: 'assets/images',
-      toType: 'dir'
-    }]),
-    new CopyWebpackPlugin([{
-      from: 'assets/third-party',
-      to: 'assets/third-party',
-      toType: 'dir'
-    }])
-  ]
-}
+	entry: ["./src/main.js"],
+	devServer: {
+		hot: true,
+		watchOptions: {
+			poll: true,
+		},
+	},
+	module: {
+		rules: [
+			{
+				test: /\.vue$/,
+				use: "vue-loader",
+			},
+			{
+				test: /\.js$/,
+				use: "babel-loader",
+			},
+			{
+				test: /\.css$/,
+				use: ["vue-style-loader", "css-loader"],
+			},
+		],
+	},
+	plugins: [
+		new webpack.HotModuleReplacementPlugin(),
+		new VueLoaderPlugin(),
+		new HtmlWebpackPlugin({
+			filename: "index.html",
+			template: "index.html",
+			inject: true,
+		}),
+		new HtmlWebpackPlugin({
+			filename: "error.html",
+			template: "error.html",
+			inject: true,
+		}),
+		new CopyWebpackPlugin([
+			{
+				from: "assets/images",
+				to: "assets/images",
+				toType: "dir",
+			},
+		]),
+	],
+};

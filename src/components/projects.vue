1e3c8a2393abeddd34e0d029b0ea1c249e98c1a0
Blake Netzeband <blake.netzeband@gmail.com>
2023-03-17 10:19:11 -0600

    refactor file structure

--- src/components/projects.vue
+++ src/components/projects.vue
@@ -1,72 +1,246 @@
 <template>
-	<section class="projects">
-		<h2>Projects</h2>
+  <section class="projects">
+    <h2>Projects</h2>
 
-		<div class="project-row" v-for="(projects, rowI) in data" :key="projects.displayName">
-			<h3>{{projects.displayName}}</h3>
+    <div
+      v-for="(projects, rowI) in data"
+      :key="projects.displayName"
+      class="project-row"
+    >
+      <h3>{{ projects.displayName }}</h3>
 
-			<div class="row-container">
-				<div
-					v-for="(project, i) in projects.data"
-					:key="projects.displayName + '-' + i"
-					class="project-square"
-					:id="getProjectId(rowI, i)"
-					@click="handleClick(rowI, i)"
-					@keyup.space.prevent="handleClick(rowI, i)"
-					:style="{ 'background': getColor(projects.rgb, i) }"
-					tabindex="1"
-				>
-					<p>{{project.name}}</p>
-				</div>
+      <div class="row-container">
+        <div
+          v-for="(project, i) in projects.data"
+          :id="getProjectId(rowI, i)"
+          :key="projects.displayName + '-' + i"
+          class="project-square"
+          :style="{ background: getColor(projects.rgb, i) }"
+          tabindex="1"
+          @click="handleClick(rowI, i)"
+          @keyup.space.prevent="handleClick(rowI, i)"
+        >
+          <p>{{ project.name }}</p>
+        </div>
 
-				<div
-					v-for="(box, i) in getGhostCount(rowI)"
-					:key="'ghost-' + i"
-					class="project-square ghost"
-					:style="{ 'background': getColor(projects.rgb, (maxBoxCount-getGhostCount(rowI) + i)) }"
-				></div>
-			</div>
+        <div
+          v-for="(box, i) in getGhostCount(rowI)"
+          :key="'ghost-' + i"
+          class="project-square ghost"
+          :style="{
+            background: getColor(
+              projects.rgb,
+              maxBoxCount - getGhostCount(rowI) + i
+            ),
+          }"
+        ></div>
+      </div>
 
-			<div
-				v-show="targetedRowID == rowI"
-				:id="getRowId(rowI)"
-				class="project-details"
-				:style="{ 'color': 'rgb(' + projects.rgb + ')' }"
-			>
-				<h3 class="project-title"></h3>
-				<p class="project-description"></p>
+      <div
+        v-show="targetedRowID == rowI"
+        :id="getRowId(rowI)"
+        class="project-details"
+        :style="{ color: 'rgb(' + projects.rgb + ')' }"
+      >
+        <h3 class="project-title"></h3>
+        <p class="project-description"></p>
 
-				<h4 class="specs">Built with:</h4>
-				<p class="project-specs specs"></p>
+        <h4 class="specs">Built with:</h4>
+        <p class="project-specs specs"></p>
 
-				<div class="project-links" :style="{ 'color': 'rgb(' + projects.rgb + ')' }">
-					<a class="link is-hidden" id="github" href target="_blank" rel="noopener noreferrer">
-						<i class="hover fa fa-github-square fa-2x" aria-hidden="true"></i>
-					</a>
-					<a class="link is-hidden" id="url" href target="_blank" rel="noopener noreferrer">
-						<i class="hover fa fa-desktop fa-2x" aria-hidden="true"></i>
-					</a>
-					<a class="link is-hidden" id="youtube" href target="_blank" rel="noopener noreferrer">
-						<i class="hover fa fa-youtube-square fa-2x" aria-hidden="true"></i>
-					</a>
-					<a class="link is-hidden" id="pics" href target="_blank" rel="noopener noreferrer">
-						<i class="hover fa fa-camera-retro fa-2x" aria-hidden="true"></i>
-					</a>
-					<a class="link is-hidden" id="linkedin" href target="_blank" rel="noopener noreferrer">
-						<i class="hover fa fa-linkedin-square fa-2x" aria-hidden="true"></i>
-					</a>
-					<a class="link is-hidden" id="behance" href target="_blank" rel="noopener noreferrer">
-						<i class="hover fa fa-behance-square fa-2x" aria-hidden="true"></i>
-					</a>
-				</div>
-			</div>
-		</div>
+        <div
+          class="project-links"
+          :style="{ color: 'rgb(' + projects.rgb + ')' }"
+        >
+          <a
+            id="github"
+            class="link is-hidden"
+            href=""
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <i class="hover fa fa-github-square fa-2x" aria-hidden="true"></i>
+          </a>
+          <a
+            id="url"
+            class="link is-hidden"
+            href=""
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <i class="hover fa fa-desktop fa-2x" aria-hidden="true"></i>
+          </a>
+          <a
+            id="youtube"
+            class="link is-hidden"
+            href=""
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <i class="hover fa fa-youtube-square fa-2x" aria-hidden="true"></i>
+          </a>
+          <a
+            id="pics"
+            class="link is-hidden"
+            href=""
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <i class="hover fa fa-camera-retro fa-2x" aria-hidden="true"></i>
+          </a>
+          <a
+            id="linkedin"
+            class="link is-hidden"
+            href=""
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <i class="hover fa fa-linkedin-square fa-2x" aria-hidden="true"></i>
+          </a>
+          <a
+            id="behance"
+            class="link is-hidden"
+            href=""
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <i class="hover fa fa-behance-square fa-2x" aria-hidden="true"></i>
+          </a>
+        </div>
+      </div>
+    </div>
 
-		<div class="projects-footer">
-			<img src="/assets/images/mountain.png" alt="distressed image of mountain">
-		</div>
-	</section>
+    <div class="projects-footer">
+      <img
+        src="~/assets/images/mountain.png"
+        alt="distressed image of mountain"
+      />
+    </div>
+  </section>
 </template>
 
-<script type="text/javascript" src="./projects.js"></script>
-<style src="../../assets/stylesheets/projects.css" scoped></style>
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+import liveProjects from '~/assets/data/project-live'
+import schoolProjects from '~/assets/data/project-school'
+import funProjects from '~/assets/data/project-fun'
+
+interface Data {
+  displayName: string
+  data: ProjectData[]
+  rgb: string
+}
+
+interface ProjectSectionData {
+  targetedRowID?: number
+  targetedProject: ProjectData
+  data: Data[]
+  maxBoxCount: number
+}
+
+const data: Data[] = [
+  {
+    displayName: 'In The Wild',
+    data: liveProjects,
+    rgb: '208, 143, 140',
+  },
+  {
+    displayName: 'Escuela',
+    data: schoolProjects,
+    rgb: '78, 111, 133',
+  },
+  {
+    displayName: 'Just For Fun',
+    data: funProjects,
+    rgb: '208, 168, 117',
+  },
+]
+
+const minBoxCount = 8 // hard coded for now. not really needed to loop through every project
+const maxBoxCount =
+  process.client && window.innerWidth / 128 > minBoxCount
+    ? (window.innerWidth - 64) / 128 // (window - padding) / projectSquare
+    : minBoxCount
+
+export default defineComponent({
+  name: 'ProjectsSection',
+
+  data(): ProjectSectionData {
+    return {
+      targetedRowID: undefined,
+      targetedProject: {} as ProjectData,
+      data,
+      maxBoxCount,
+    }
+  },
+
+  mounted() {
+    this.$ScrollReveal().reveal(
+      '.project-square',
+      {
+        duration: 1000,
+        delay: 250,
+        origin: 'right',
+      },
+      50
+    )
+  },
+
+  methods: {
+    getColor: function (rgb: string, i: number) {
+      return 'rgba(' + rgb + ',' + (1 - i / this.maxBoxCount) + ')'
+    },
+    getGhostCount(i: number) {
+      return Math.floor(
+        this.maxBoxCount - Object.keys(this.data[i].data).length
+      )
+    },
+    getRowId(i: number) {
+      return this.data[i].displayName.toLowerCase().replace(/\s/g, '-')
+    },
+    getProjectId(rowI: number, projI: number) {
+      return this.data[rowI].data[projI].name.toLowerCase().replace(/\s/g, '-')
+    },
+    handleClick: function (rowI: number, i: number) {
+      this.targetedProject = this.data[rowI].data[i]
+      this.targetedRowID = rowI
+
+      const el = document.querySelector<HTMLElement>('#' + this.getRowId(rowI))
+      if (el) {
+        this.updateDetailSection(el)
+      }
+    },
+    updateDetailSection: function (el: HTMLElement) {
+      // update name, description and specs (if project has specs)
+      el.querySelector('.project-title')!.innerHTML = this.targetedProject.name
+      el.querySelector('.project-description')!.innerHTML =
+        this.targetedProject.description
+      if (this.targetedProject.specs) {
+        el.querySelector('.project-specs')!.innerHTML =
+          this.targetedProject.specs
+      } else {
+        el.querySelectorAll('.specs').forEach((el) =>
+          el.classList.add('is-hidden')
+        )
+      }
+      // update links
+      if (this.targetedProject.links) {
+        const domLinks = el.querySelectorAll('.link')
+        const projectLinks = Object.keys(this.targetedProject.links)
+        for (let i = domLinks.length - 1; i >= 0; i--) {
+          const domLink = domLinks[i]
+          const possibleLink = this.targetedProject.links[domLink.id as Link]
+          if (projectLinks.includes(domLink.id) && possibleLink) {
+            domLinks[i].setAttribute('href', possibleLink)
+            domLink.classList.remove('is-hidden')
+          } else {
+            domLink.classList.add('is-hidden')
+          }
+        }
+      }
+    },
+  },
+})
+</script>
+<style src="~/assets/stylesheets/projects.css" scoped></style>
--- src/components/skills.vue
+++ src/components/skills.vue
@@ -1,51 +1,185 @@
 <template>
-	<section class="skills">
-		<canvas id="paper-canvas-skills" resize="true"></canvas>
+  <section class="skills">
+    <canvas id="paper-canvas-skills" resize="true"></canvas>
 
-		<div class="skills-header">
-			<div class="skills-sr">
-				<div class="skills-text">
-					<h2>Skills</h2>
-					<p id="skills-tip">(Feel free to click around...)</p>
-				</div>
-				<img src="/assets/images/hand.png" alt="a retro-styled image of a pointing hand">
-			</div>
-		</div>
+    <section class="skills-header">
+      <div class="skills-sr">
+        <div class="skills-heading">
+          <h2>Skills</h2>
+          <p id="skills-tip">(Feel free to click around...)</p>
+        </div>
+        <img
+          src="~/assets/images/hand.png"
+          alt="a retro-styled image of a pointing hand"
+        />
+      </div>
+      <p class="skills-text">
+        There's 2 ways to go about this section. I could either list every bit
+        of tech I've touched, which would probably be outdated by the time you
+        read it. Or I could give you the ten-thousand foot view. I think I'll go
+        with the later.
+      </p>
+      <p class="skills-text">
+        At this point in my career, I consider myself in expert in web
+        technologies, especially those leaning towards the front-end.
+      </p>
+      <ul>
+        <li>
+          I have extensive experience with React and its forever expanding
+          ecosystem. I've created and deployed countless apps and mentored
+          countless more developers in React. I've professional experience with
+          numerous React-adjacent tools, such as React Native, Next.js,
+          developing UI libraries; and React project skills such as crafting
+          custom build tools, architecting mono-repos and other complex apps,
+          etc.
+        </li>
+        <li>
+          I have professional experience in UI/UX design, including asset
+          creation, developing mock-ups for stakeholders and running usability
+          tests. I believe UI/UX includes the consideration of
+          <i>all</i> users, which is why I make all my apps WCAG compliant.
+        </li>
+      </ul>
+      <p class="skills-text">
+        In short, I have all the tools needed to bring an idea into a successful
+        production-ready app.
+      </p>
+    </section>
 
-		<div class="skills-row">
-			<div v-for="skillObj in data" class="skills-column" :key="skillObj.category">
-				<h3>{{skillObj.category}}</h3>
-				<ul>
-					<li v-for="item in skillObj.skills" :key="item">{{item}}</li>
-				</ul>
-			</div>
-		</div>
+    <div class="svg-wrapper">
+      <svg width="500" height="150" viewBox="0 0 500 150">
+        <defs>
+          <pattern
+            id="pattern"
+            x="0"
+            y="5"
+            width="500"
+            height="15"
+            patternUnits="userSpaceOnUse"
+          >
+            <line
+              x1="0"
+              y1="0"
+              x2="500"
+              y2="0"
+              stroke-width="10"
+              stroke="black"
+            ></line>
+          </pattern>
+        </defs>
+        <ellipse
+          cx="250"
+          cy="180"
+          rx="250"
+          ry="150"
+          fill="url(#pattern)"
+        ></ellipse>
+      </svg>
+    </div>
 
-		<div class="svg-wrapper">
-			<svg width="500" height="150" viewBox="0 0 500 150">
-				<defs>
-					<pattern id="pattern" x="0" y="5" width="500" height="15" patternUnits="userSpaceOnUse">
-						<line x1="0" y1="0" x2="500" y2="0" stroke-width="10" stroke="black"></line>
-					</pattern>
-				</defs>
-				<ellipse cx="250" cy="180" rx="250" ry="150" fill="url(#pattern)"></ellipse>
-			</svg>
-		</div>
-
-		<div class="icons">
-			<a
-				v-for="link in links"
-				:key="link.href"
-				:href="link.href"
-				target="_blank"
-				rel="noopener noreferrer"
-			>
-				<i class="hover fa fa-2x" :class="link.icon" aria-hidden="true"></i>
-			</a>
-		</div>
-	</section>
+    <div class="icons">
+      <a
+        v-for="link in links"
+        :key="link.href"
+        :href="link.href"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <i class="hover fa fa-2x" :class="link.icon" aria-hidden="true"></i>
+      </a>
+    </div>
+  </section>
 </template>
 
-<script type="text/javascript" src="./skills.js"></script>
+<script lang="ts">
+import paper from 'paper'
+import { defineComponent } from 'vue'
 
-<style src="../../assets/stylesheets/skills.css" scoped></style>
+import data from '~/assets/data/skills-skills'
+
+interface SkillsDataType {
+  data: SkillData[]
+  links: SkillLink[]
+  path: paper.Path | null
+}
+
+const links: SkillLink[] = [
+  {
+    href: 'https://github.com/blakenetz',
+    icon: 'fa-github',
+  },
+  {
+    href: 'https://medium.com/@blakenetz',
+    icon: 'fa-medium',
+  },
+  {
+    href: 'https://www.behance.net/blakenetz',
+    icon: 'fa-behance',
+  },
+  {
+    href: 'https://www.linkedin.com/in/blakenetz',
+    icon: 'fa-linkedin',
+  },
+  {
+    href: 'mailto:blake.netzeband@gmail.com',
+    icon: 'fa-envelope',
+  },
+]
+
+const scope = new paper.PaperScope()
+scope.setup('paper-canvas-skills')
+
+export default defineComponent({
+  name: 'SkillsSection',
+
+  data(): SkillsDataType {
+    return {
+      data,
+      links,
+      path: null,
+    }
+  },
+
+  mounted() {
+    scope.activate()
+
+    this.path = new scope.Path({
+      strokeColor: 'rgba(138, 254, 225, 0.5)',
+      strokeWidth: 15,
+      strokeCap: 'round',
+    })
+
+    scope.view.onClick = (e: paper.MouseEvent) => {
+      if (this.path) {
+        this.path.add(e.point)
+      }
+    }
+
+    scope.view.onDoubleClick = (_e: paper.MouseEvent) => {
+      if (this.path?.strokeColor) {
+        this.path.strokeColor.hue = Math.random() * 360
+      }
+    }
+
+    this.$ScrollReveal().reveal(
+      '.skills-sr',
+      {
+        origin: 'right',
+        duration: 2000,
+        distance: '100%',
+        mobile: false,
+        viewFactor: 0.5,
+        afterReveal: function (el: HTMLElement) {
+          const img = el.querySelector('img')
+          if (img) {
+            img.classList.add('animate')
+          }
+        },
+      },
+      50
+    )
+  },
+})
+</script>
+
+<style src="~/assets/stylesheets/skills.css" scoped></style>

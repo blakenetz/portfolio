2456a5a4918da32290560c48cda48304d74c557e
Blake Netzeband <blake.netzeband@gmail.com>
2024-02-15 14:10:26 -0700

    update types

--- types/paper.ts
+++ types/paper.ts
@@ -1,10 +1,19 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable @typescript-eslint/no-namespace */
+
+type ViewEvent = {
+  /** the number of times the frame event was fired */
+  count: number;
+  /** the total amount of time passed since the first frame event in seconds */
+  delta: number;
+  /** the time passed in seconds since the last frame event */
+  time: number;
+};
 declare namespace paper {
   interface Item {
     segments: paper.Segment[];
   }
   interface View {
-    onFrame: (e: { count: number }) => void;
+    onFrame: (e: ViewEvent) => void;
   }
 }

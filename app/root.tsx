983dace63ff422f37e030aeba5bd47cc3e6a4497
Blake Netzeband <blake.netzeband@gmail.com>
2024-02-24 11:55:59 -0700

    remove error boundary

--- app/root.tsx
+++ app/root.tsx
@@ -12,14 +12,7 @@ import type {
   LinksFunction,
   MetaFunction,
 } from "@remix-run/node";
-import {
-  Links,
-  LiveReload,
-  Meta,
-  Outlet,
-  Scripts,
-  useRouteError,
-} from "@remix-run/react";
+import { Links, LiveReload, Meta, Outlet, Scripts } from "@remix-run/react";
 
 import styles from "~/styles/root.css";
 
@@ -93,24 +86,6 @@ const resolver: CSSVariablesResolver = (theme) => {
     dark: {},
   };
 };
-
-export function ErrorBoundary() {
-  const error = useRouteError();
-  console.error(error);
-  return (
-    <html lang="en">
-      <head>
-        <title>Oh no!</title>
-        <Meta />
-        <Links />
-      </head>
-      <body>
-        {/* add the UI you want your users to see */}
-        <Scripts />
-      </body>
-    </html>
-  );
-}
 
 export default function App() {
   const [ada, setAda] = useLocalStorage({

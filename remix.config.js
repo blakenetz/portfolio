e426d668a05641f57c1fd290676f7922b965accc
Blake Netzeband <blake.netzeband@gmail.com>
2024-02-25 16:14:50 -0700

    refactor to arc

--- remix.config.js
+++ remix.config.js
@@ -2,7 +2,12 @@
 export default {
   ignoredRouteFiles: ["**/.*"],
   postcss: true,
-  cacheDirectory: '.cache'
+  cacheDirectory: '.cache',
+  // arc
+  publicPath: "/_static/build/",
+  server: "server.ts",
+  serverBuildPath: "server/index.mjs",
+  serverModuleFormat: "esm",
   // appDirectory: "app",
   // assetsBuildDirectory: "public/build",
   // publicPath: "/build/",

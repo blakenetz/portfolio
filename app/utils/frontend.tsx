d2f0b5d5c9f4173dba0f238da314a3bb23e26394
Blake Netzeband <blake.netzeband@gmail.com>
2024-08-01 12:52:16 -0600

    remove unused component

--- app/utils/frontend.tsx
+++ app/utils/frontend.tsx
@@ -1,12 +1,3 @@
-import { compile } from "@mdx-js/mdx";
-
 export function cls(...args: (string | boolean | undefined | null)[]) {
   return args.filter(Boolean).join(" ").trim();
-}
-
-export async function createMDXComponent(mdx: string) {
-  const code = await compile(mdx, { outputFormat: "function-body" });
-  const results = eval(code.value.toString());
-  console.log(results);
-  return results;
 }

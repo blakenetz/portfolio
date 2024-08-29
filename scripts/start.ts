125056e45cb3ee3e6635b1f8ebddd9561e2ed5ee
Blake Netzeband <blake.netzeband@gmail.com>
2024-08-29 14:21:56 -0600

    fix start script

--- scripts/start.ts
+++ scripts/start.ts
@@ -9,39 +9,39 @@ if (!mode) throw new Error("pass a mode argument!");
 
 // Function to run the cleanup script
 async function cleanup() {
-  await DB.close();
-  console.log("🌻 Clean up finished!");
+	await DB.close();
+	console.log("🌻 Clean up finished!");
 }
 
 // Handling process termination signals
 process.on("SIGINT", () => {
-  console.log("🌻 Received SIGINT. Running cleanup...");
-  cleanup().finally(() => process.exit());
+	console.log("🌻 Received SIGINT. Running cleanup...");
+	cleanup().finally(() => process.exit());
 });
 
 process.on("SIGTERM", () => {
-  console.log("🌻 Received SIGTERM. Running cleanup...");
-  cleanup().finally(() => process.exit());
+	console.log("🌻 Received SIGTERM. Running cleanup...");
+	cleanup().finally(() => process.exit());
 });
 
 // Start the Remix Vite server
 console.log(`🌻 Starting vite server in ${mode} mode`);
 
 const options: SpawnOptions = {
-  stdio: "inherit",
-  env: { ...process.env, NODE_ENV: "production" },
+	stdio: "inherit",
+	env: { ...process.env, NODE_ENV: mode as NodeJS.ProcessEnv["NODE_ENV"] },
 };
 
 const args =
-  mode === "production"
-    ? ["remix-serve", "./build/server/index.js"]
-    : ["remix", "vite:dev"];
+	mode === "production"
+		? ["remix-serve", "./build/server/index.js"]
+		: ["remix", "vite:dev"];
 
 const vite = spawn("npx", args, options);
 
 vite.on("close", (code) => {
-  console.log(
-    `🌻 Remix Vite server exited with code ${code}. Running cleanup...`
-  );
-  cleanup();
+	console.log(
+		`🌻 Remix Vite server exited with code ${code}. Running cleanup...`
+	);
+	cleanup();
 });

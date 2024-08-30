f4a415bb86d772147f0c35fb0932eba3257e187a
Blake Netzeband <blake.netzeband@gmail.com>
2024-08-29 22:02:52 -0600

    added share section. update meta elements

--- app/routes/blog.$post/route.tsx
+++ app/routes/blog.$post/route.tsx
@@ -1,20 +1,21 @@
 import { Flex } from "@mantine/core";
 import {
-	ActionFunctionArgs,
-	json,
-	LoaderFunctionArgs,
-	MetaFunction,
-	redirect,
+  ActionFunctionArgs,
+  json,
+  LoaderFunction,
+  LoaderFunctionArgs,
+  MetaFunction,
+  redirect,
 } from "@remix-run/node";
 import { Link, useLoaderData } from "@remix-run/react";
 import Markdown from "react-markdown";
 import remarkFrontmatter from "remark-frontmatter";
 import remarkMdxFrontmatter from "remark-mdx-frontmatter";
-import { Mdx } from "types/modules";
 
 import { Button } from "~/components";
 import { authenticator } from "~/server/authenticator.server";
 import { getPost, postComment } from "~/server/blog.server";
+import { PostModel } from "~/server/db.singleton.server";
 import commonStyles from "~/styles/common.module.css";
 import { cls } from "~/utils";
 
@@ -24,63 +25,71 @@ import styles from "./post.module.css";
 import Share from "./share";
 import Source from "./source";
 
-const posts = import.meta.glob<Mdx>("/app/blog/*.mdx", {
-	eager: true,
-});
+export const meta: MetaFunction<LoaderFunction> = ({ data, location }) => {
+  const { meta } = data as PostModel;
 
-export const meta: MetaFunction = ({ location }) => {
-	const module = posts[`/app${location.pathname}.mdx`];
-
-	return module?.meta;
+  return [
+    meta,
+    /** @see https://www.linkedin.com/help/linkedin/answer/a521928/making-your-website-shareable-on-linkedin?lang=en */
+    { property: "og:title", content: meta.title },
+    { property: "og:description", content: meta.description },
+    {
+      property: "og:url",
+      content: "https://blakenetzeband.com" + location.pathname,
+    },
+  ];
 };
 
 export async function loader({ request, params }: LoaderFunctionArgs) {
-	const results = await getPost(request, params);
-	const user = await authenticator.isAuthenticated(request);
+  const results = await getPost(request, params);
+  const user = await authenticator.isAuthenticated(request);
+  const url = new URL(request.url);
+  const shareUrl = url.origin + url.pathname;
 
-	if (results.ok === false) {
-		return redirect(`/blog?status=${results.errorStatus}`);
-	}
+  if (results.ok === false) {
+    return redirect(`/blog?status=${results.errorStatus}`);
+  }
 
-	return json({
-		user,
-		comments: results.comments,
-		commentsTotal: results.commentsTotal,
-		meta: results.post.meta,
-		component: results.post.content.toString(),
-	});
+  return json({
+    user,
+    comments: results.comments,
+    commentsTotal: results.commentsTotal,
+    meta: results.post.meta,
+    component: results.post.content.toString(),
+    shareUrl,
+  });
 }
 
 export async function action({ request, params }: ActionFunctionArgs) {
-	const results = await postComment(request, params);
+  const results = await postComment(request, params);
 
-	return json(results);
+  return json(results);
 }
 
 export default function Post() {
-	const { user, meta, component, comments, commentsTotal } =
-		useLoaderData<typeof loader>();
+  const { user, meta, component, comments, commentsTotal, shareUrl } =
+    useLoaderData<typeof loader>();
 
-	return (
-		<>
-			<Source meta={meta} />
+  return (
+    <>
+      <Source meta={meta} />
 
-			<Flex className={cls(commonStyles.column, styles.reader)}>
-				<Markdown
-					components={components}
-					remarkPlugins={[remarkFrontmatter, remarkMdxFrontmatter]}
-				>
-					{component}
-				</Markdown>
-			</Flex>
+      <Flex className={cls(commonStyles.column, styles.reader)}>
+        <Markdown
+          components={components}
+          remarkPlugins={[remarkFrontmatter, remarkMdxFrontmatter]}
+        >
+          {component}
+        </Markdown>
+      </Flex>
 
-			<Share />
+      <Share url={shareUrl} meta={meta} />
 
-			<Comments user={user} comments={comments} commentsTotal={commentsTotal} />
+      <Comments user={user} comments={comments} commentsTotal={commentsTotal} />
 
-			<Button component={Link} to="/blog">
-				Take me back
-			</Button>
-		</>
-	);
+      <Button component={Link} to="/blog">
+        Take me back
+      </Button>
+    </>
+  );
 }
--- app/routes/blog.$post/share.tsx
+++ app/routes/blog.$post/share.tsx
@@ -1,13 +1,59 @@
-import { Title } from "@mantine/core";
+import { Button, Title } from "@mantine/core";
+import { SerializeFrom } from "@remix-run/node";
+import { IconBrandLinkedin, IconBrandReddit } from "@tabler/icons-react";
 
+import { PostModel } from "~/server/db.singleton.server";
 import { cls } from "~/utils";
 
 import styles from "./post.module.css";
 
-export default function Share() {
-	return (
-		<section className={cls(styles.flex, styles.comments)}>
-			<Title order={3}>Share</Title>
-		</section>
-	);
+interface ShareProps {
+  /**
+   * URL used for share links
+   */
+  url: string;
+  /** Meta data of blog post */
+  meta: SerializeFrom<PostModel["meta"]>;
+}
+
+export default function Share({ url, meta }: ShareProps) {
+  const linkedinHref =
+    "http://www.linkedin.com/shareArticle?" +
+    new URLSearchParams({
+      mini: "true",
+      url,
+    }).toString();
+
+  const redditHref =
+    "https://www.reddit.com/submit?" +
+    new URLSearchParams({
+      url,
+      title: meta.title,
+      type: "LINK",
+    }).toString();
+
+  return (
+    <section className={cls(styles.flex, styles.comments)}>
+      <Title order={3}>Share</Title>
+      <div className={cls(styles.flex, styles.row)}>
+        <Button
+          leftSection={<IconBrandLinkedin />}
+          component="a"
+          target="_blank"
+          href={linkedinHref}
+        >
+          LinkedIn
+        </Button>
+
+        <Button
+          leftSection={<IconBrandReddit />}
+          component="a"
+          target="_blank"
+          href={redditHref}
+        >
+          Reddit
+        </Button>
+      </div>
+    </section>
+  );
 }

ac304046c85b0105b4b8c3598637cd77565cf3b7
Blake Netzeband <blake.netzeband@gmail.com>
2025-02-21 13:59:39 -0700

    fine tune checkboard. added animate color event

--- apps/astro/src/consts.ts
+++ apps/astro/src/consts.ts
@@ -4,3 +4,5 @@
 export const SITE_TITLE = "Blake Netzeband";
 export const SITE_DESCRIPTION =
   "Hi! I'm Blake and this is my chunk of the internet";
+
+export const ANIMATE_COLORS_CLASSNAME = "animate-colors";

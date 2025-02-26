31f520cd514f8aa01f61657a1cd719a5560fe795
Blake Netzeband <blake.netzeband@gmail.com>
2025-02-25 17:35:08 -0700

    added styles to project screen

--- apps/astro/src/consts.ts
+++ apps/astro/src/consts.ts
@@ -6,3 +6,4 @@ export const SITE_DESCRIPTION =
   "Hi! I'm Blake and this is my chunk of the internet";
 
 export const ANIMATE_COLORS_CLASSNAME = "animate-colors";
+export const ANIMATE_COLORS_CLASSES = `${ANIMATE_COLORS_CLASSNAME} text-primary cursor-help font-danfo`;

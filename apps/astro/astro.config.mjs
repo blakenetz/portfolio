265690495afb3a57a2ef6dac00c15bd885585984
Blake Netzeband <blake.netzeband@gmail.com>
2025-03-04 08:57:38 -0700

    added cloudflare adaptor

--- apps/astro/astro.config.mjs
+++ apps/astro/astro.config.mjs
@@ -5,14 +5,20 @@ import sitemap from "@astrojs/sitemap";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
 
+import cloudflare from "@astrojs/cloudflare";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://blakenetzeband.com",
   integrations: [mdx(), sitemap()],
+
   vite: {
     plugins: [tailwindcss()],
   },
+
   experimental: {
     svg: true,
   },
+
+  adapter: cloudflare(),
 });

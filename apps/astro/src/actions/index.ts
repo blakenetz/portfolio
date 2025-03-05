82df4e94b12832e11442c53a3e3f56215202a485
Blake Netzeband <blake.netzeband@gmail.com>
2025-03-05 09:25:42 -0700

    refactor sort elements to own component

--- apps/astro/src/actions/index.ts
+++ apps/astro/src/actions/index.ts
@@ -1,4 +1,11 @@
 import { defineAction } from "astro:actions";
 import { z } from "astro:schema";
 
-export const server = {};
+export const server = {
+  sortGithubProjects: defineAction({
+    input: z.object({
+      sort: z.enum(["asc", "desc"]),
+    }),
+    handler: async ({ sort }) => sort,
+  }),
+};

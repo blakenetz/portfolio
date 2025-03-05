b3272cbd498079a9ee62c490a4389e0307889265
Blake Netzeband <blake.netzeband@gmail.com>
2025-03-05 10:59:43 -0700

    clean up props

--- apps/astro/src/actions/index.ts
+++ apps/astro/src/actions/index.ts
@@ -3,9 +3,10 @@ import { z } from "astro:schema";
 
 export const server = {
   sortGithubProjects: defineAction({
-    input: z.object({
-      sort: z.enum(["asc", "desc"]),
-    }),
-    handler: async ({ sort }) => sort,
+    input: z.object({ direction: z.string() }),
+    handler: async ({ direction }) => {
+      console.log("inside handler", direction);
+      return direction;
+    },
   }),
 };

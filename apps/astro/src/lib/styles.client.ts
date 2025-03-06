2f9a5b20e28fe2019c070abab8245e696d899365
Blake Netzeband <blake.netzeband@gmail.com>
2025-03-05 20:37:43 -0700

    refactor to update gh repo item on sort

--- apps/astro/src/lib/styles.client.ts
+++ apps/astro/src/lib/styles.client.ts
@@ -1,3 +1,5 @@
+import type { Color } from "~/types";
+
 export function getCssVariable(name: string) {
   return getComputedStyle(document.documentElement).getPropertyValue(name);
 }
@@ -6,3 +8,24 @@ export const white = getCssVariable("--color-white");
 export const black = getCssVariable("--color-black");
 const checkerboardSize = getCssVariable("--spacing-checkerboard-size");
 export const spacing = parseInt(checkerboardSize) / 2;
+
+export function getLanguageColor(language: string) {
+  let color: Color = "aquamarine";
+  switch (language) {
+    case "TypeScript":
+    case "JavaScript":
+      color = "dodger-blue";
+      break;
+    case "Just":
+      color = "rich-black";
+      break;
+    case "CSS":
+    case "HTML":
+      color = "sunglow";
+      break;
+    case "Ruby":
+      color = "rose";
+  }
+
+  return getCssVariable(`--color-${color}`);
+}
--- apps/astro/src/types/webComponents.ts
+++ apps/astro/src/types/webComponents.ts
@@ -1,4 +1,9 @@
+import type { RepoData } from "~/types/projects";
+
 export interface SnackbarElement extends HTMLElement {
   show: (timeout?: number) => void;
   hide: () => void;
+}
+export interface GitHubProjectsGridItem extends HTMLElement {
+  update: (repo: RepoData[number]) => void;
 }

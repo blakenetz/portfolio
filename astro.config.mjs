daa582aadbaa3699380115f65ff7169bbc025752
Blake Netzeband <blake.netzeband@gmail.com>
2025-03-06 15:31:06 -0700

    cleanup unused cloudflare adaptor

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,8 +5,6 @@ import sitemap from "@astrojs/sitemap";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
 
-import cloudflare from "@astrojs/cloudflare";
-
 // https://astro.build/config
 export default defineConfig({
   site: "https://blakenetzeband.com",
@@ -19,6 +17,4 @@ export default defineConfig({
   experimental: {
     svg: true,
   },
-
-  adapter: cloudflare(),
 });
